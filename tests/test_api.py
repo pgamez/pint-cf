c1f4be777a5eda60b6200b59e69ab18422f9ca4a
Pedro Gámez <pgamez@meteosim.com>
2026-04-08 08:21:04 +0000

    remove redundant tests

--- tests/test_api.py
+++
@@ -1,9 +0,0 @@
-from pint_cf import cf_unitregistry
-
-
-def test_cf_unitregistry_registers_cf_formatter() -> None:
-    ureg = cf_unitregistry()
-    q = ureg("1 m/s^2")
-
-    assert f"{q:cf}" == "1.0 meter-second^-2"
-    assert f"{q:~cf}" == "1.0 m/s2"
