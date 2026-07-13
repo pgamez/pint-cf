048f5e72068fbdf5e404ab4c3142d1281a610d6b
Pedro Gámez <pgamez@meteosim.com>
2026-07-13 05:25:20 +0000

    git ignore

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Eliminar el repositorio de Yarn que causa problemas de verificación GPG
-sudo rm -f /etc/apt/sources.list.d/yarn.list
-
 sudo apt update
 sudo apt install -y vim libudunits2-dev plantuml
 
